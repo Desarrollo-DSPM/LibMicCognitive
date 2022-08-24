c2edc33d6599363741a965460cabd2d4208d1a82
WaterBlueNewWorld <alejandroj@tuta.io>
2022-08-23 18:18:50 -0600

    Cambios

---
+++ LibMicCognitive/Credentials.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMicCognitive
+{
+    public class Credentials
+    {
+        public static string AzureKey => "";
+
+        public static string Region => "";
+
+        public static string SpeechRecognitionRegion => "";
+    }
+}
